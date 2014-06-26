02c7f3ffc775299e9d4ca4d1af0993c090690145
sorin <sorin.nica@woowteam.com>
2014-06-26 18:42:46 +0300

    work on collision detection

--- src/fractal/games/swipe/sorin/petre/nica/math/geometry/shapes/CenteredDrawable.java
+++ src/fractal/games/swipe/sorin/petre/nica/math/geometry/shapes/CenteredDrawable.java
@@ -11,56 +11,61 @@ import fractal.games.swipe.sorin.petre.nica.math.objects.Point2D;
 
 public abstract class CenteredDrawable extends Drawable {
 
-    protected static final Paint DEFAULT_PAINT;
-    static {
-        DEFAULT_PAINT = new Paint();
-        DEFAULT_PAINT.setColor(Color.WHITE);
-        DEFAULT_PAINT.setStyle(Style.STROKE);
-        DEFAULT_PAINT.setStrokeWidth(4);
-    }
+	protected static final Paint	DEFAULT_PAINT;
+	static {
+		DEFAULT_PAINT = new Paint();
+		DEFAULT_PAINT.setColor(Color.WHITE);
+		DEFAULT_PAINT.setStyle(Style.STROKE);
+		DEFAULT_PAINT.setStrokeWidth(4);
+	}
 
-    private Point2D              center;
+	private Point2D					center;
 
-    protected Paint              paint;
+	protected Paint					paint;
 
-    public Integer               boundingBoxRight;
+	public Integer					boundingBoxRight;
 
-    public CenteredDrawable(Point2D center, Paint paint) {
-        this.center = center;
-        this.paint = new Paint();
-        this.paint.setColor(paint.getColor());
-        this.paint.setStyle(paint.getStyle());
-        this.paint.setStrokeWidth(paint.getStrokeWidth());
-        this.paint.setAntiAlias(true);
-        this.paint.setDither(true);
-    }
+	public CenteredDrawable(Point2D center, Paint paint) {
+		this.center = center;
+		this.paint = new Paint();
+		this.paint.setColor(paint.getColor());
+		this.paint.setStyle(paint.getStyle());
+		this.paint.setStrokeWidth(paint.getStrokeWidth());
+		this.paint.setAntiAlias(true);
+		this.paint.setDither(true);
+	}
 
-    public abstract void onMotionEvent(MotionEvent motionEvent);
+	public abstract void onMotionEvent(MotionEvent motionEvent);
 
-    public abstract void updateState(Long elapsedTime);
+	public abstract void updateState(Long elapsedTime);
 
-    public void setCenter(Point2D newCenter) {
-        center = newCenter;
-    }
+	public void setCenter(Point2D newCenter) {
+		center = newCenter;
+	}
 
-    public Point2D getCenter() {
-        return center;
-    }
+	public Point2D getCenter() {
+		return center;
+	}
 
-    @Override
-    public int getOpacity() {
-        return PixelFormat.OPAQUE;
-    }
+	@Override
+	public int getOpacity() {
+		return PixelFormat.OPAQUE;
+	}
 
-    @Override
-    public void setAlpha(int alpha) {
-        paint.setAlpha(alpha % 256);
-    }
+	@Override
+	public void setAlpha(int alpha) {
+		paint.setAlpha(alpha % 256);
+	}
 
-    @Override
-    public void setColorFilter(ColorFilter cf) {
-        // TODO Auto-generated method stub
+	@Override
+	public void setColorFilter(ColorFilter cf) {
+		// TODO Auto-generated method stub
 
-    }
+	}
+
+	public void onCollision(CenteredDrawable obstacle) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- src/fractal/games/swipe/sorin/petre/nica/math/geometry/shapes/Rectangle.java
+++ src/fractal/games/swipe/sorin/petre/nica/math/geometry/shapes/Rectangle.java
@@ -1,5 +1,8 @@
 package fractal.games.swipe.sorin.petre.nica.math.geometry.shapes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -9,95 +12,118 @@ import fractal.games.swipe.sorin.petre.nica.math.objects.Point2D;
 
 public class Rectangle extends AnimatedShape {
 
-    private static final double COLLISION_SPEED_LOSS = 2.0;
+	private static final double			COLLISION_SPEED_LOSS	= 2.0;
 
-    private Boolean             isFilled;
+	private Boolean						isFilled;
 
-    private Double              width;
+	private Double						width;
 
-    public Double               height;
+	public Double						height;
 
-    private Bitmap              bitmap;
+	private Bitmap						bitmap;
 
-    public Rectangle(Point2D center, Double width, Double height, Paint paint) {
-        super(center, paint);
-        this.width = width;
-        this.height = height;
-        isFilled = false;
-    }
+	public final Set<CenteredDrawable>	obstacles;
 
-    public Rectangle(Point2D center, Double width, Double height) {
-        this(center, width, height, DEFAULT_PAINT);
-    }
+	public Rectangle(Point2D center, Double width, Double height, Paint paint) {
+		super(center, paint);
+		this.width = width;
+		this.height = height;
+		isFilled = false;
+		obstacles = new HashSet<CenteredDrawable>();
+	}
 
-    public Rectangle(Point2D center, Integer width, Integer height) {
-        this(center, width.doubleValue(), height.doubleValue());
-    }
+	public Rectangle(Point2D center, Double width, Double height) {
+		this(center, width, height, DEFAULT_PAINT);
+	}
 
-    public void setBitmap(Bitmap bitmap) {
-        this.bitmap = Bitmap.createScaledBitmap(bitmap, width.intValue(), height.intValue(), true);
-    }
+	public Rectangle(Point2D center, Integer width, Integer height) {
+		this(center, width.doubleValue(), height.doubleValue());
+	}
 
-    public Boolean isFilled() {
-        return isFilled;
-    }
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = Bitmap.createScaledBitmap(bitmap, width.intValue(), height.intValue(), true);
+	}
 
-    public void setFilled(Boolean isFilled) {
-        paint.setStyle(isFilled ? Style.FILL : Style.STROKE);
-        this.isFilled = isFilled;
-    }
+	public Boolean isFilled() {
+		return isFilled;
+	}
 
-    @Override
-    public void draw(Canvas canvas) {
-        if (bitmap == null) {
-            canvas.drawRect(getCenter().getX() - (width.floatValue() / 2), getCenter().getY() - (height.floatValue() / 2), getCenter().getX() + (width.floatValue() / 2), getCenter().getY() + (height.floatValue() / 2),
-                    paint);
-        } else {
-            canvas.drawBitmap(bitmap, getCenter().getX() - (width.floatValue() / 2), getCenter().getY() - (height.floatValue() / 2), paint);
-        }
-    }
+	public void setFilled(Boolean isFilled) {
+		paint.setStyle(isFilled ? Style.FILL : Style.STROKE);
+		this.isFilled = isFilled;
+	}
 
-    @Override
-    public void updateState(Long elapsedTime) {
-        super.updateState(elapsedTime);
+	@Override
+	public void draw(Canvas canvas) {
+		if (bitmap == null) {
+			canvas.drawRect(getCenter().getX() - (width.floatValue() / 2), getCenter().getY() - (height.floatValue() / 2), getCenter().getX() + (width.floatValue() / 2),
+					getCenter().getY() + (height.floatValue() / 2), paint);
+		} else {
+			canvas.drawBitmap(bitmap, getCenter().getX() - (width.floatValue() / 2), getCenter().getY() - (height.floatValue() / 2), paint);
+		}
+	}
 
-        if (boundingBoxRight != null) {
-            if (crossedLeftSideBoundry()) {
-                moveToLeftSideBoundry();
-                reverseVelocityAlongX();
-            }
-            if (crossedRightSideBoundry()) {
-                moveToRightSideBoundry();
-                reverseVelocityAlongX();
-            }
-        }
-    }
+	private CenteredDrawable checkPossibleCollisions() {
+		for (CenteredDrawable obstacle : obstacles) {
+			if (obstacle instanceof Rectangle) {
+				Rectangle rectangleObstacle = (Rectangle) obstacle;
 
-    private void reverseVelocityAlongX() {
-        velocity.reverseX();
-        velocity.divideXByScalar(COLLISION_SPEED_LOSS);
-    }
+			}
+		}
+		return null;
+	}
 
-    private void moveToRightSideBoundry() {
-        setCenter(new Point2D(boundingBoxRight - (width / 2), getCenter().getY().doubleValue()));
-    }
+	@Override
+	public void updateState(Long elapsedTime) {
+		super.updateState(elapsedTime);
 
-    private boolean crossedRightSideBoundry() {
-        return getCenter().getX() + (width / 2) > boundingBoxRight;
-    }
+		if (boundingBoxRight != null) {
+			if (crossedLeftSideBoundry()) {
+				moveToLeftSideBoundry();
+				reverseVelocityAlongX();
+			}
+			if (crossedRightSideBoundry()) {
+				moveToRightSideBoundry();
+				reverseVelocityAlongX();
+			}
+		}
 
-    private void moveToLeftSideBoundry() {
-        setCenter(new Point2D((width / 2), getCenter().getY().doubleValue()));
-    }
+		CenteredDrawable colidedObstacle = checkPossibleCollisions();
+		moveOutsideBoundriesOfObstacle(colidedObstacle);
+		onCollision(colidedObstacle);
+		colidedObstacle.onCollision(this);
+	}
 
-    private boolean crossedLeftSideBoundry() {
-        return getCenter().getX() - (width / 2) < 0;
-    }
+	private void moveOutsideBoundriesOfObstacle(CenteredDrawable colidedObstacle) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void onMotionEvent(MotionEvent motionEvent) {
-        // TODO Auto-generated method stub
+	}
 
-    }
+	private void reverseVelocityAlongX() {
+		velocity.reverseX();
+		velocity.divideXByScalar(COLLISION_SPEED_LOSS);
+	}
+
+	private void moveToRightSideBoundry() {
+		setCenter(new Point2D(boundingBoxRight - (width / 2), getCenter().getY().doubleValue()));
+	}
+
+	private boolean crossedRightSideBoundry() {
+		return getCenter().getX() + (width / 2) > boundingBoxRight;
+	}
+
+	private void moveToLeftSideBoundry() {
+		setCenter(new Point2D((width / 2), getCenter().getY().doubleValue()));
+	}
+
+	private boolean crossedLeftSideBoundry() {
+		return getCenter().getX() - (width / 2) < 0;
+	}
+
+	@Override
+	public void onMotionEvent(MotionEvent motionEvent) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
